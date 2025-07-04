b1ade7275e95ca22082252bc25a5a60b38b25423
Lertchao <64230092@dpu.ac.th>
2025-07-04 19:42:55 +0700

    edit link db

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const MongoStore = require("connect-mongo");
 //mongodb+srv://64230092:1234@cluster0.pglvr.mongodb.net/
 //mongodb://localhost:27017/Database
 
-const dbUrl = 'mongodb+srv://64230092:1234@cluster0.pglvr.mongodb.net'; 
+const dbUrl = 'mongodb+srv://64230092:1234@cluster0.pglvr.mongodb.net/'; 
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true
